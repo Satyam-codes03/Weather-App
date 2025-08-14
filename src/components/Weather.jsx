057d049b3d72d74f3eb1ffe313d5d6bb43a078f3
Satyam-codes03 <satyampandeyofficial05@gmail.com>
2025-08-14 11:18:15 +0530

    created Weather component and mounted it

---
+++ src/components/Weather.jsx
@@ -0,0 +1,19 @@
+// we have created component as we have to create a weather app in center of the web page
+// so in src folder we have created component folder now in this components we will create Weather.jsx file, always start file name with capital letter so that ki ham jab component banayenge toh wo component ko baad me capital se start na karna pade  
+// now type rafce to get the basic functional react component and for this component we will add css file also so for that we have Weather.css 
+// to link this css add import statement and provide the path './Weather.css'
+// add 1 text <h1>Weather</h1> in this div 
+// now we have to mount this Weather component to app component so open app .jsx file and within this div there add <Weather/> this will import the weather component by itself from component folder in app.jsx 
+
+import React from 'react'
+
+import './Weather.css'
+const Weather = () => {
+  return (
+    <div>
+        <h1>Weather</h1>
+    </div>
+  )
+}
+
+export default Weather
